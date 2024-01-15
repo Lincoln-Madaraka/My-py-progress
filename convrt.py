0722400f76f2f215299a960dff77b739dce9b572
Lincoln-Madaraka <madarakalincoln48@gmail.com>
2024-01-15 12:06:23 +0300

    my commit

---
+++ convrt.py
@@ -0,0 +1,6 @@
+miles = input('ENTER MILES: ')
+#convert string to integer
+miles = int(miles)
+#convert miles to km by mult
+kilometres = miles * 1.861
+print('{} miles equals {} kilometres'.format(miles, kilometres))
