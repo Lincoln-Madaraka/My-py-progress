c026942474d7dc3ff278ed4faf59ba0ffd4d4161
Lincoln-Madaraka <madarakalincoln48@gmail.com>
2024-01-26 13:34:33 +0300

    commit

---
+++ logicals.py
@@ -0,0 +1,10 @@
+#we test the use of logical operators in this one
+#inpu age and convert to integers,at once, intro to the eval func
+age = eval(input("ENTER AGE: "))
+#if age is less then 16, 50 and <65 its important 
+if (age<=16) or (age==50) or (age<65):
+ print("Important Birthday")
+else:
+ print("Not important")
+
+#Disclaimer this is just an example don't misinterpret
